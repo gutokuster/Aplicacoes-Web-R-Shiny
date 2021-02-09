bcad3b96046d80ca9c48276bf3ba31dd6499cea5
Guto Kuster <gutokuster@gmail.com>
2021-02-08 21:10:17 -0300

    Caso #5

--- Aplicacoes/05-avaliando-a-probabilidade-de-quebra-de-equipamentos-em-uma-industria/README.md
+++ Aplicacoes/05-avaliando-a-probabilidade-de-quebra-de-equipamentos-em-uma-industria/README.md
@@ -1,5 +1,4 @@
 
-
 # Caso #5 - Avaliando a Probabilidade de Quebra de Equipamentos em uma Indústria
 **Fev/21**
 
@@ -8,12 +7,12 @@
 
 Uma indústria tem vários equipamentos críticos na sua linha de produção e precisa de uma aplicação que possa calcular a probabilidade de quebras.
 
-Exemplo: Se em médiao equipametno quebra duas  ano, a empresa quer saber qual a probabilidade de quebrar 1, 3, 4 ou nenhuma vez ao ano.
+Exemplo: Se em média o equipamento quebra duas vezes ao ano, a empresa quer saber qual a probabilidade de quebrar 1, 3, 4 ou nenhuma vez ao ano.
 
 
 #### Técnica utilizada
 
-- Distribuição de Poison: Mede a probabilidade de ocorrência de eventos (que devem ser independentes) em um intervalo de tempo.
+- Distribuição de Poisson: Mede a probabilidade de ocorrência de eventos (que devem ser independentes) em um intervalo de tempo.
 
 #### Fonte de dados
 
--- Aplicacoes/05-avaliando-a-probabilidade-de-quebra-de-equipamentos-em-uma-industria/app.R
+++ Aplicacoes/05-avaliando-a-probabilidade-de-quebra-de-equipamentos-em-uma-industria/app.R
@@ -4,11 +4,51 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Probabilidade de Falhas em Equipamentos"),
+    fluidRow(
+        column(6, 
+               radioButtons("opcao", "Selecione o tipo de cálculo", choices=list("Probabilidade Exata"=1, "Menor que"=2, "Maior que"=3), selected =1)
+               ),
+        column(6, 
+               numericInput("ocorrencia", "Ocorrência Atual:", value=2, min=1, max=99),
+               actionButton("processar", "processar")
+               )
+    ),
+    fluidRow(
+        column(12, 
+               plotOutput("graficoProbabilidades")
+               )
+    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    observeEvent(input$processar,{
+        lamb = input$ocorrencia
+        tipoCalc = input$opcao
+        inicio = lamb - 2
+        fim = lamb + 2
+        if (tipoCalc == 1) {
+            x = dpois(inicio:fim, lambda = lamb)
+            titulo = "Probabilidade de Ocorrência"
+        }
+        if (tipoCalc == 2) {
+            x = ppois(inicio:fim, lambda = lamb)
+            titulo = "Probabilidade de Ocorrência Menor que"
+        }
+        if (tipoCalc == 3) {
+            x = ppois(inicio:fim, lambda = lamb, lower.tail = F)
+            titulo = "Probabilidade de Ocorrência Maior que"
+        }
+        
+        z = as.character(round(x,4))
+        y = as.character(inicio:fim)
+        
+        lab = paste(y, "Probabilidade:", z)
+        output$graficoProbabilidades = renderPlot({
+            barplot(x,names.arg = lab, col = gray.colors(5), main=titulo)
+        })
+    })
 
 }
 
